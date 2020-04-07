9791098ce1b4c6f54ea67f87f28037e222fb0542
sairam4123 <opencity@info.github.com>
2020-04-07 17:06:53 +0530

    fixed mainmenu

--- opencity_kivy/main_menu.py
+++ opencity_kivy/main_menu.py
@@ -1,8 +1,5 @@
 import kivy
 import os
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.image import Image
-from opencity_kivy.hoverbehavior import HoverBehavior
 from kivy.app import App
 from kivy.config import Config
 
