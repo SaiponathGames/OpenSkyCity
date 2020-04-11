643939690913132b1139f9e91bf1426ca2e1ae44
sairam4123 <opencity@info.github.com>
2020-04-11 11:35:00 +0530

    update 1.4.9 fix

--- file/file.py
+++ file/file.py
@@ -1,40 +1,28 @@
-def file_read(x):
-	f1 = open(x, 'r')
-	list1 = []
-	for a in f1:
-		list1.append(a)
-	f1.close()
-	return list1
+def file_read(input_file):
+	with open(input_file, "r") as __in__:
+		return [data for data in __in__]
 
 
-def file_write(x, y):
-	f1 = open(x, 'w')
-	f1.write(y)
-	f1.close()
+def file_write(input_file, content):
+	with open(input_file, "w") as __in__:
+		__in__.write(content)
 
 
-def file_append(x, y):
-	f1 = open(x, 'a')
-	f1.write(y)
-	f1.close()
+def file_append(input_file, content):
+	with open(input_file, "a") as __in__:
+		__in__.write(content)
 
 
-def binary_file_read(x):
-	f2 = open(x, 'rb')
-	list2 = []
-	for a in f2:
-		list2.append(a)
-	f2.close()
-	return list2
+def binary_file_read(input_file):
+	with open(input_file, "rb") as __in__:
+		return [bytes_1 for bytes_1 in __in__]
 
 
-def binary_file_write(x, y):
-	f2 = open(x, 'wb')
-	f2.write(y)
-	f2.close()
+def binary_file_write(input_file, bytes_1):
+	with open(input_file, "wb") as __in__:
+		__in__.write(bytes_1)
 
 
-def binary_file_append(x, y):
-	f2 = open(x, 'ab')
-	f2.write(y)
-	f2.close()
+def binary_file_append(input_file, bytes_1):
+	with open(input_file, "ab") as __in__:
+		__in__.write(bytes_1)
--- files_and_folders/files_and_folder_creator.py
+++ files_and_folders/files_and_folder_creator.py
@@ -16,26 +16,27 @@ def folder_and_file_creator():
 	home_dirs, appdata_dirs = dirs.dirs_reader('paths.txt')
 	dirs.dirs_creator(home_dirs)
 	dirs.dirs_creator(appdata_dirs)
-	file_list = files.file_reader('files.txt')
+	file_list = files.files_reader('files.txt')
 	os.chdir(original_path)
 	os.chdir(home_dirs[0])
-	files.file_checker(file_list[0])
+	files.files_checker(file_list[0])
 	os.chdir(home_dirs[1])
-	files.file_checker(file_list[2])
+	files.files_checker(file_list[2])
 	os.chdir(appdata_dirs[0])
-	files.file_checker(file_list[1])
+	files.files_checker(file_list[1])
 	os.chdir(appdata_dirs[1])
-	files.file_checker(file_list[3])
+	files.files_checker(file_list[3])
 	os.chdir(home_dirs[8])
 	for i in range(10):
-		files.file_checker(file_list[5].format(i))
+		files.files_checker(file_list[5].format(i))
 	os.chdir(home_dirs[6])
 	for i in range(5):
 		if len(os.listdir(home_dirs[6])) < 5:
 			mapname = random.choice(mapnames)
-			files.file_checker(file_list[4].format(mapname))
+			files.files_checker(file_list[4].format(mapname))
 		else:
 			break
+
 
 
 if __name__ == '__main__':
